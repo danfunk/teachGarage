62df3dc33856e48b64883b0ec011e01977d2e06d
Dan Funk <dan.funk@twintechs.com>
2015-10-21 16:39:22 -0400

    Just cleaning up this file, I hope to implement real endpoints at some point in the future.

--- src/main/scala/GarageMicroservice.scala
+++ src/main/scala/GarageMicroservice.scala
@@ -1,6 +1,14 @@
-/**
- * Created by dan on 10/20/15.
- */
-class GarageMicroservice {
 
+// This allows us to implicitly marshal case classes to json when a
+// function declares a return type of Option[T]Future[T]
+import actors.LevelActor
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+
+/**
+ * Please Ignore this file for now, and view the Level and Spot actors.
+ * I plan to flesh this out in a separate branch, and would be happy to
+ * share it as I make more progress.
+ */
+
+class GarageMicroservice {
 }
